f99488dca8822470cef416ed3fcadd362c0c7e1e
Jatin Chawda <jztinchawda@gmail.com>
2022-07-26 00:17:04 +0200

    updated path

--- fakenews/inference.py
+++ fakenews/inference.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sys
-sys.path.insert(0, '..')
+path = sys.path[-1]
 from fakenews.preprocess import clean_data,preprocess_data,vectorize_data,select_features,vectorize_data,get_prediction
 import joblib
 
@@ -11,7 +11,7 @@ def make_predictions(data):
     X =select_features(data,['title','author','text'])
     X = preprocess_data(X)
     X = vectorize_data(X,'test')
-    model = joblib.load('../models/XGBClassifier.joblib')
+    model = joblib.load(path +'fakenews/models/XGBClassifier.joblib')
     y_pred = get_prediction(model,X)
     return y_pred
 
